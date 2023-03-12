ba6698a1a8667f7e5dcb3406178e7e2610d7d61a
AyjayJr <ayjayjr@hotmail.com>
2023-03-12 19:08:37 -0400

    added a card component and styling

--- src/components/Users/AddUser.js
+++ src/components/Users/AddUser.js
@@ -1,4 +1,7 @@
 import React from 'react';
+import Card from '../UI/Card';
+
+import classes from './AddUser.module.css';
 
 const AddUser = props => {
 	const addUserHandler = e => {
@@ -6,13 +9,15 @@ const AddUser = props => {
 	}
 
 	return (
-		<form>
-			<label htmlFor='username'>Username</label>
-			<input id='username' type='text' />
-			<label htmlFor='age'>Age</label>
-			<input id='age' type='number' />
-			<button onClick={addUserHandler} type='submit'>Add User</button>
-		</form>
+		<Card className={classes.input}>
+			<form>
+				<label htmlFor='username'>Username</label>
+				<input id='username' type='text' />
+				<label htmlFor='age'>Age</label>
+				<input id='age' type='number' />
+				<button onClick={addUserHandler} type='submit'>Add User</button>
+			</form>
+		</Card>
 	);
 };
 
