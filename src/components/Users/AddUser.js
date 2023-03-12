c4f56a65461570ef01ab251ad440d2d187e52914
AyjayJr <ayjayjr@hotmail.com>
2023-03-12 19:56:08 -0400

    created states to store user input and errors alerts for invalid input

--- src/components/Users/AddUser.js
+++ src/components/Users/AddUser.js
@@ -1,21 +1,53 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Card from '../UI/Card';
 import Button from '../UI/Button';
 
 import classes from './AddUser.module.css';
 
 const AddUser = props => {
+	const [enteredUsername, setEnteredUsername] = useState('');
+	const [enteredAge, setEnteredAge] = useState('');
+
 	const addUserHandler = e => {
 		e.preventDefault();
+		if (enteredAge < 0) {
+			alert('Age must be a number larger than 0!');
+			return;
+		}
+		if (enteredUsername === '' || enteredAge === '') {
+			alert('All fields must be filled!');
+			return;
+		}
+		setEnteredAge('');
+		setEnteredUsername('');
+		console.log(enteredUsername, enteredAge);
 	}
+
+	const usernameChangeHandler = e => {
+		setEnteredUsername(e.target.value);
+	};
+
+	const ageChangeHandler = e => {
+		setEnteredAge(e.target.value);
+	};
 
 	return (
 		<Card className={classes.input}>
 			<form>
 				<label htmlFor='username'>Username</label>
-				<input id='username' type='text' />
+				<input
+					value={enteredUsername}
+					id='username'
+					type='text'
+					onChange={usernameChangeHandler}
+				/>
 				<label htmlFor='age'>Age</label>
-				<input id='age' type='number' />
+				<input
+					value={enteredAge}
+					id='age'
+					type='number'
+					onChange={ageChangeHandler}
+				/>
 				<Button onClick={addUserHandler} type='submit'>Add User</Button>
 			</form>
 		</Card>
